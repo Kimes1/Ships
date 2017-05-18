b543c22c69ec546698445beb2775270f420bcd4d
Kimes1 <kimesik@gmail.com>
2017-05-19 00:06:53 +0200

    first commit

---
+++ src/main/java/com/niedzielski/Ships.java
@@ -0,0 +1,12 @@
+package com.niedzielski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ships {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ships.class, args);
+	}
+}
